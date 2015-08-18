3c24d0dec5376dce2a3dfc23ad17385bb9b5728c
MrAnts <Romeoyap@hotmail.com>
2015-08-19 00:34:05 +0800

    HengSoon's 1st Version
    DON'T FORK OR DOWNLOAD THIS

---
+++ SP1Framework-master/SP1Framework/game.cpp
@@ -0,0 +1,341 @@
+// This is the main file for the game logic and function
+//
+//
+#include "game.h"
+#include "Framework\console.h"
+#include <iostream>
+#include <iomanip>
+
+double elapsedTime;
+double deltaTime;
+bool keyPressed[K_COUNT];
+COORD charLocation;
+COORD consoleSize;
+
+//HengSoon Code Start
+
+	enum myType
+	{
+		boundry,
+		enemy,
+		colorModifier,
+		player,
+	};
+
+	struct Player
+	{
+		myType type;
+		int hp = 3;
+		Player()
+		{
+			type = player;
+			colour(0x1A);
+		}
+	};
+
+	struct AI
+	{
+		myType type;
+		COORD Coord;
+		int speed = 1;
+
+		// constructor
+		AI()
+		{
+			type = enemy;
+			colour(0x1A);
+		}
+	};
+
+	AI LRAI1;
+	AI UDAI1;
+	AI UDLRAI1;
+
+	int direction = -1;  // -1 = going left, 1 = going right
+	int direction2 = -1; // -1 = going up, 1 = going down
+	int direction3 = -1; // -1 = going up or left, 1 = going down or right
+
+	bool goingRight = false, goingLeft = true, goingUp = false, goingDown = false;
+
+	int map[60][25] = {
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // left to right total 60 lines
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,//  top to bottom total 25 lines
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+	};
+
+//HengSoon Code End
+
+void init()
+{
+    // Set precision for floating point output
+    std::cout << std::fixed << std::setprecision(3);
+
+    SetConsoleTitle(L"RGB Maze");
+
+    // Sets the console size, this is the biggest so far.
+    setConsoleSize(80, 30);
+
+    // Get console width and height
+    CONSOLE_SCREEN_BUFFER_INFO csbi; /* to get buffer info */     
+
+    /* get the number of character cells in the current buffer */ 
+    GetConsoleScreenBufferInfo( GetStdHandle( STD_OUTPUT_HANDLE ), &csbi );
+    consoleSize.X = csbi.srWindow.Right + 1;
+    consoleSize.Y = csbi.srWindow.Bottom + 1;
+
+    // set the character to be in the center of the screen.
+    charLocation.X = consoleSize.X / 2;
+    charLocation.Y = consoleSize.Y / 2;
+
+//HengSoon Code Start
+
+	LRAI1.Coord.X = 60;
+	LRAI1.Coord.Y = 1;
+
+	UDAI1.Coord.X = 45;
+	UDAI1.Coord.Y = 15;
+
+	UDLRAI1.Coord.X = 20;
+	UDLRAI1.Coord.Y = 20;
+
+//HengSoon Code End
+
+    elapsedTime = 0.0;
+}
+
+void shutdown()
+{
+    // Reset to white text on black background
+	colour(FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
+}
+
+void getInput()
+{    
+    keyPressed[K_UP] = isKeyPressed(VK_UP);
+    keyPressed[K_DOWN] = isKeyPressed(VK_DOWN);
+    keyPressed[K_LEFT] = isKeyPressed(VK_LEFT);
+    keyPressed[K_RIGHT] = isKeyPressed(VK_RIGHT);
+    keyPressed[K_ESCAPE] = isKeyPressed(VK_ESCAPE);
+}
+
+//HengSoon Code Start
+void AIMovementLR(COORD curPlaceX)
+{
+	int Direction = -1;
+	COORD* NextX = 0;
+
+	NextX = &curPlaceX;
+	NextX += Direction;
+
+	if (UDLRAI1.Coord.X < 0 || UDLRAI1.Coord.X > 80)
+	{
+		Direction *= -1;
+	}
+}
+void AIMovementUD(COORD curPlaceY)
+{
+	int Direction = -1;
+	COORD* NextY = 0;
+
+	NextY = &curPlaceY;
+	NextY += Direction;
+
+	if (UDLRAI1.Coord.Y < 1 || UDLRAI1.Coord.Y == 30)
+	{
+		Direction *= -1;
+	}
+}
+void AIMovementUDLR(COORD curPlaceX, COORD curPlaceY)
+{
+	int Direction = -1;
+	COORD* NextX = 0;
+	COORD* NextY = 0;
+	NextX = &curPlaceX;
+	NextY = &curPlaceY;
+
+	NextX += Direction;
+	if (NextX < 0)
+	{
+		Direction *= -1;
+	}
+}
+//HengSoon Code End
+
+void update(double dt)
+{
+
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo( GetStdHandle( STD_OUTPUT_HANDLE ), &csbi );
+
+    // get the delta time
+    elapsedTime += dt;
+    deltaTime = dt;
+
+//HengSoon Code Start
+	AIMovementLR(LRAI1.Coord.X);
+	AIMovementUD(UDAI1.Coord.Y);
+
+	//LRAI1.Coord.X+= direction;
+	//UDAI1.Coord.Y += direction2;
+
+	if(goingRight)
+	{
+		direction3 = 1;
+		UDLRAI1.Coord.X += direction3;
+		if (UDLRAI1.Coord.X == csbi.srWindow.Right)
+		{
+			UDLRAI1.Coord.X = csbi.srWindow.Right;
+			goingRight = false;
+			goingDown = true;
+		}
+	}
+
+	if(goingLeft)
+	{
+		direction3 = -1;
+		UDLRAI1.Coord.X += direction3;
+		if (UDLRAI1.Coord.X == csbi.srWindow.Left)
+		{
+			UDLRAI1.Coord.X = csbi.srWindow.Left;
+			goingLeft = false;
+			goingUp = true;
+		}
+	}
+
+	if(goingUp)
+	{
+		direction3 = -1;
+		UDLRAI1.Coord.Y += direction3;
+		if (UDLRAI1.Coord.Y < 1)
+		{
+			goingUp = false;
+			goingRight = true;
+		}
+	}
+
+	if(goingDown)
+	{
+		direction3 = 1;
+		UDLRAI1.Coord.Y += direction3;
+		if (UDLRAI1.Coord.Y > 10)
+		{
+			UDLRAI1.Coord.Y += direction3;
+			goingDown = false;
+			goingLeft = true;
+		}
+	}
+
+	if (UDLRAI1.Coord.X < 0 || UDLRAI1.Coord.X > csbi.srWindow.Right)
+	{
+		direction *= -1;
+	}
+
+	if (UDLRAI1.Coord.Y < 1 || UDLRAI1.Coord.Y == csbi.srWindow.Bottom)
+	{
+		direction2 *= -1;
+	}
+	
+//HengSoon Code End
+	
+    // Updating the location of the character based on the key press
+    if (keyPressed[K_UP] && charLocation.Y > 0)
+    {
+        Beep(1440, 30);
+        charLocation.Y--; 
+    }
+    if (keyPressed[K_LEFT] && charLocation.X > 0)
+    {
+        Beep(1440, 30);
+        charLocation.X--; 
+    }
+    if (keyPressed[K_DOWN] && charLocation.Y < consoleSize.Y - 1)
+    {
+        Beep(1440, 30);
+        charLocation.Y++; 
+    }
+    if (keyPressed[K_RIGHT] && charLocation.X < consoleSize.X - 1)
+    {
+        Beep(1440, 30);
+        charLocation.X++; 
+    }
+
+    // quits the game if player hits the escape key
+    if (keyPressed[K_ESCAPE])
+        g_quitGame = true;    
+}
+
+void render()
+{
+    // clear previous screen
+    colour(0x0F);
+    cls();
+
+    //render the game
+
+    //render test screen code (not efficient at all)
+    const WORD colors[] =   {
+	                        0x1A, 0x2B, 0x3C, 0x4D, 0x5E, 0x6F,
+	                        0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF6
+	                        };
+	
+	for (int i = 0; i < 12; ++i)
+	{
+		gotoXY(3*i,i+1);
+		colour(colors[i]);
+		std::cout << "O";
+	}
+
+    // render time taken to calculate this frame
+    gotoXY(70, 0);
+    colour(0x1A);
+    std::cout << 1.0 / deltaTime << "fps" << std::endl;
+  
+    gotoXY(0, 0);
+    colour(0x59);
+    std::cout << elapsedTime << "secs" << std::endl;
+
+    // render character
+    gotoXY(charLocation);
+    colour(0x0C);
+    std::cout << (char)1;
+
+//HengSoon Code Start
+
+	// render enemies
+	gotoXY(LRAI1.Coord);
+	colour(0x0C);
+	std::cout << (char)29;
+
+	gotoXY(UDAI1.Coord);
+	colour(0x0C);
+	std::cout << (char)29;
+
+	gotoXY(UDLRAI1.Coord);
+	colour(0x0C);
+	std::cout << (char)29;
+
+//HengSoon Code End
+    
+}
